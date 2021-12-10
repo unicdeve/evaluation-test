2f2dbcecefbe8cb9c2e03cfcf173d98f94cec3a8
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-12-10 23:35:04 +0100

    feat: register models on admin panel

--- survey/admin.py
+++ survey/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Answer, Survey, SurveyQuestion
+
 # Register your models here.
+admin.site.register(Survey)
+admin.site.register(SurveyQuestion)
+admin.site.register(Answer)
